0f125a77d5c9b79992b702e1213e12fd2c67c393
f4deb <f4deb1@gmail.com>
2020-07-19 19:29:58 +0200

    DRIVER MCP23017 READ & WRITE OK

--- drivers/mcpIoExpander/MCP23017.c
+++ drivers/mcpIoExpander/MCP23017.c
@@ -28,14 +28,14 @@ Mcp23017Data* _readPcm23017MotorI2c(MotorI2c* motorI2c) {
     portableMasterWaitSendI2C(i2cBusConnection);
     portableMasterStartI2C(i2cBusConnection);
     WaitI2cBusConnection(i2cBusConnection);
-    portableMasterWriteI2C(i2cBusConnection, MCP23017_ADDRESS_7);
+    portableMasterWriteI2C(i2cBusConnection, mcp23017Data->mcp23017Address);
     WaitI2cBusConnection(i2cBusConnection);
-    portableMasterWriteI2C(i2cBusConnection, 0x12);
+    portableMasterWriteI2C(i2cBusConnection, mcp23017Data->mcp23017Register);
     WaitI2cBusConnection(i2cBusConnection);
     
     portableMasterStartI2C(i2cBusConnection);
     WaitI2cBusConnection(i2cBusConnection);
-    portableMasterWriteI2C(i2cBusConnection, MCP23017_ADDRESS_7 +1);
+    portableMasterWriteI2C(i2cBusConnection, mcp23017Data->mcp23017Address +1);
     WaitI2cBusConnection(i2cBusConnection);
     mcp23017Data->mcp23017Data = portableMasterReadI2C(i2cBusConnection);
     portableMasterNackI2C(i2cBusConnection);
